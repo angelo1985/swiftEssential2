6feff6f02d65bd8dedce571ba910033606eebc5e
angelo1985 <angelosus30@gmail.com>
2019-12-08 23:59:16 +0100

    Initial Commit

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Ilaria Dot on 08/12/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+
+
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
